90ab24536d7b2198f002477f543c49a1451af869
Bad-Raider <stanislav.gusiev@gmail.com>
2023-03-19 14:33:57 +0200

    task-1
    task-1 - done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,47 @@
+// randon color
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+// DOM elements
+const btnStartEl = document.querySelector("[data-start]");
+const btnStopEl = document.querySelector("[data-stop]");
+const bodyEl = document.body;
+
+let interval = null;
+
+addedDisableAttribute(btnStopEl);
+
+btnStartEl.addEventListener("click", handleStartChangingColorBody);
+// handler "btnStartEl"
+function handleStartChangingColorBody() {
+    addedDisableAttribute(btnStartEl);
+    removeDisableAttribute(btnStopEl);
+
+    return interval = setInterval(() => {
+        changeColor();
+    }, 1000);
+};
+
+btnStopEl.addEventListener("click", handleStopChangingColorBody);
+// handler "btnStopEl"
+function handleStopChangingColorBody() {
+    clearInterval(interval);
+    addedDisableAttribute(btnStopEl);
+    removeDisableAttribute(btnStartEl);
+}
+
+function changeColor() {
+    const color = getRandomHexColor();
+    bodyEl.style.backgroundColor = color;
+};
+
+function addedDisableAttribute(element) {
+    element.setAttribute("disabled", true);
+};
+
+function removeDisableAttribute(element) {
+    element.removeAttribute("disabled");
+};
+
+
 
